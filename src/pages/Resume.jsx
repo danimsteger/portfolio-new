2e42da36a51fe9b05f91b751451b619e4e55c91b
danimsteger <daniellestegertutoring@gmail.com>
2024-08-15 18:17:07 -0400

    resume proficiencies added

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,13 +2,62 @@ export default function Resume() {
   return (
     <div>
       <h1 className="text-center"> Resume</h1>
-      <p>
+      <p className="text-center">
         Download a PDF of Danielles Resume&nbsp;
         <a href="public/resume.portfolio.pdf" target="_blank" download>
           here
         </a>
         .
       </p>
+
+      <h3>Front-End Proficiencies</h3>
+      <button type="button" className="btn btn-outline-secondary">
+        HTML
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        CSS
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        JavaScript
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        jQuery
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        React
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        Bootstrap
+      </button>
+
+      <h3>Back-End Proficiencies</h3>
+      <button type="button" className="btn btn-outline-secondary">
+        APIs
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        Node
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        Express
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        MySQL
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        Sequelize
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        MongoDB
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        Mongoose
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        REST
+      </button>
+      <button type="button" className="btn btn-outline-secondary">
+        GraphQL
+      </button>
     </div>
   );
 }
